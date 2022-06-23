165b123d29a05623e3bc6fcbe6adf60202bc9bad
Nick <mr.upgrade3@protonmail.com>
2022-06-24 02:35:18 +0300

    Trying to find answer for make 10x10 table with 0 to 100

--- script/script.js
+++ script/script.js
@@ -1,34 +1,49 @@
 
 function generate_table() {
 
-    // Create a <table> element and a <tbody> element
-
-    const tbl = document.createElement("table");
-    const tblBody = document.createElement("tbody");
-
-    // Creating all cells
-
-    for (let i = 1; i < 101; i++) {
-
-        // Create a table row
-
-        const row = document.createElement("tr");
-
-        for (let j = 0; j < 1; j++) {
-
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-
-            const cell = document.createElement("td");
-            const cellText = document.createTextNode(i);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+    // Create additional function for cells
+    function newFunc() {
+        let counter = 1;
+        while (counter <= 99) {
+            console.log(counter);
+            counter = counter + 1;
         }
+        return counter;
+    }
 
-        // Add the row to the end of table body
+        let checkVar = newFunc();
 
-        tblBody.appendChild(row);
+
+        // Create a <table> element and a <tbody> element
+
+        const tbl = document.createElement("table");
+        const tblBody = document.createElement("tbody");
+
+        // Creating all cells
+
+        for (let i = 1; i < 11; i++) {
+
+            // Create a table row
+
+            const row = document.createElement("tr");
+
+            for (let j = 0; j < 10; j++) {
+
+                // Create a <td> element and a text node, make the text
+                // node the contents of the <td>, and put the <td> at
+                // the end of the table row
+
+
+                const cell = document.createElement("td");
+                const cellText = document.createTextNode(newFunc);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+
+            // Add the row to the end of table body
+
+            tblBody.appendChild(row);
+
     }
 
     // Put the <tbody> in the <table>
@@ -39,9 +54,8 @@ function generate_table() {
 
     document.body.appendChild(tbl);
 
-    // Sets the border attribute of tbl to "2"
+    // Sets the border attribute of tbl to "1"
 
     tbl.setAttribute("border", "1");
-
 
 }
