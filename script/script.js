b63cfd89cdaf934e75b4bb38b52c60437329a1f5
Nick <mr.upgrade3@protonmail.com>
2022-06-27 20:48:48 +0300

    Find solution

--- script/script.js
+++ script/script.js
@@ -1,61 +1,18 @@
 
 function generate_table() {
-
-    // Create additional function for cells
-    function newFunc() {
-        let counter = 1;
-        while (counter <= 99) {
-            console.log(counter);
-            counter = counter + 1;
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+    for (let y = 0; y < 10; y++) {
+        const row = document.createElement("tr");
+        for (let x = 0; x < 10; x++) {
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode((x + 10 * y) + 1);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
         }
-        return counter;
+        tblBody.appendChild(row);
     }
-
-        let checkVar = newFunc();
-
-
-        // Create a <table> element and a <tbody> element
-
-        const tbl = document.createElement("table");
-        const tblBody = document.createElement("tbody");
-
-        // Creating all cells
-
-        for (let i = 1; i < 11; i++) {
-
-            // Create a table row
-
-            const row = document.createElement("tr");
-
-            for (let j = 0; j < 10; j++) {
-
-                // Create a <td> element and a text node, make the text
-                // node the contents of the <td>, and put the <td> at
-                // the end of the table row
-
-
-                const cell = document.createElement("td");
-                const cellText = document.createTextNode(newFunc);
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-
-            // Add the row to the end of table body
-
-            tblBody.appendChild(row);
-
-    }
-
-    // Put the <tbody> in the <table>
-
     tbl.appendChild(tblBody);
-
-    // Appends <table> into <body>
-
     document.body.appendChild(tbl);
-
-    // Sets the border attribute of tbl to "1"
-
     tbl.setAttribute("border", "1");
-
 }
